b89d47ab0f2b29177f55fc87964659c2ef36054b
sonwaneakash <42178604+sonwaneakash@users.noreply.github.com>
2018-10-12 23:45:58 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,20 @@
+//this is the library file
+#include<iostream>
+using namespace std;
+//the function for printing all the numbers
+int qw(int a,int b)
+{
+ if (a>b) {
+	return 0;}
+ else {cout<< a <<endl;
+	a++;
+	qw(a,b);
+	return 0;}
+}
+//the main function
+int main(){
+int d;
+cout<<"Type the number upto which you want all numbers printed"<<endl;
+cin>>d;
+qw(1,d);
+return 0;}
